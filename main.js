185eb9417fe0741c5d5dee3dfa705e94a7355335
Alicia Riggs <aliciariggs.15.ar@gmail.com>
2017-03-14 12:15:03 -0700

    update

--- main.js
+++ main.js
@@ -14,7 +14,7 @@
 			Sorry, but I meant a REAL headache, your first day is Feb 27th! HAVE FUN!	
 	NO - Well here's some prework but your first day is tomorrow, have fun!
 */
-
+/*
 var welcome = alert("Hello! Welcome to your future!")
 var name = prompt("My name is Mac, what is your name?")
 var welcome = alert("Welcome to the world of coding " + name +"!")
@@ -29,11 +29,11 @@ var positive = confirm("Do you know what you are doing here?")
 			if (positive) {
 				var brainHurt = prompt("Do you come with experience or none at all?")
 				
-				if (brainHurt = "Experience") {
+				if (brainHurt == "Experience") {
 					alert("OOOOH your poor brain! Okay your first day is in 10 mins!")
 					alert(" I'll send you the prework make sure its done before you get to class, it should take you about 7 hrs!")
 					alert("Good luck!")
-				} else if(brainHurt = "None at all") {
+				} else if(brainHurt == "None at all") {
 					alert("Yay! We can welcome you to a REAL headache in just a bit!!!")
 					alert("I'll download some prework ...")
 					alert("Your first class is right now, prework should be done by then!!")
@@ -41,3 +41,125 @@ var positive = confirm("Do you know what you are doing here?")
 			}
 		}
 	}
+*/
+
+
+
+/*
+var welcome = alert("Hello! Welcome to your future!")
+
+var name = prompt("My name is Mac, what is your name?")
+
+var welcome = alert("Welcome to the world of coding " + name +"!")
+
+var positive = confirm("Do you know what you are doing here?")
+    if (positive) {
+        //yes
+        var name = confirm("Are you ready to make your brain hurt?")
+
+       if (positive) {
+            var welcome = confirm("Are you sure you want to keep going?")
+
+           if (positive) {
+
+             var brainHurt = prompt("Do you come with experience or none at all?")
+
+               if (brainHurt == "Experience") {
+
+                   alert("OOOOH your poor brain! Okay your first day is in 10 mins!")
+
+                   alert(" I'll send you the prework make sure its done before you get to class, it should take you about 7 hrs!")
+
+                   alert("Good luck!")
+
+               } else if(brainHurt == "None at all") {
+
+                   alert("Yay! We can welcome you to a REAL headache in just a bit!!!")
+
+                   alert("I'll download some prework ...")
+
+                   alert("Your first class is right now, prework should be done by then!!")
+
+               }
+            }
+        }
+    }
+*/
+
+var welcome = alert("Hello! Welcome to your future!")
+
+var name = prompt("My name is Mac, what is your name?")
+
+var welcome = alert("Welcome to the world of coding " + name +"!")
+
+var positive = confirm("Do you know what you are doing here?")
+    if (positive) {
+        //yes
+        var name = confirm("Are you ready to make your brain hurt?")
+
+       if (positive) {
+            var welcome = confirm("Are you sure you want to keep going?")
+
+           if (positive) {
+
+             var brainHurt = prompt("Do you come with experience or none at all? \n (0) - None at all\n (1) - Experience")
+
+               if (brainHurt == 0) {
+
+                   alert("OOOOH your poor brain! Okay your first day is in 10 mins!")
+
+                   alert(" I'll send you the prework make sure its done before you get to class, it should take you about 7 hrs!")
+
+                   alert("Good luck!")
+
+               } else if (brainHurt == 1) {
+
+                   alert("Yay! We can welcome you to a REAL headache in just a bit!!!")
+
+                   alert("I'll download some prework ...")
+
+                   alert("Your first class is right now, prework should be done by then!!")
+
+               }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
