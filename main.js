c3ee21ff53cb1a5315f86facf086608e718ec2d2
Alicia Riggs <aliciariggs.15.ar@gmail.com>
2017-04-21 13:57:01 -0700

    update

--- main.js
+++ main.js
@@ -3,127 +3,64 @@
 
  "Hello! I'm your Mac, what's your name?"
  "Welcome to the world of coding! Do you know what you're doing?"
- 	YES - Are you ready to make your brain hurt?
- 		YES - Are you sure you want to do that?
- 			YES - !Welcome them in!
- 				Do you come with experience or inexperience?
- 				Experience - Haven't you put your brain through a lot already!?
- 				Inexperience - Get ready for a lifetime of headaches, pain, and tears!
-			NO - Okay, there goes your life, we'll be sure to put a close tag />
-		NO - Well, have you had a headache before?
-			Sorry, but I meant a REAL headache, your first day is Feb 27th! HAVE FUN!	
-	NO - Well here's some prework but your first day is tomorrow, have fun!
-*/
-/*
-var welcome = alert("Hello! Welcome to your future!")
-var name = prompt("My name is Mac, what is your name?")
-var welcome = alert("Welcome to the world of coding " + name +"!")
-var positive = confirm("Do you know what you are doing here?")
-	if (positive) {
-		//yes
-		var name = confirm("Are you ready to make your brain hurt?")
-
-		if (positive) {
-			var welcome = confirm("Are you sure you want to keep going?")
-
-			if (positive) {
-				var brainHurt = prompt("Do you come with experience or none at all?")
-				
-				if (brainHurt == "Experience") {
-					alert("OOOOH your poor brain! Okay your first day is in 10 mins!")
-					alert(" I'll send you the prework make sure its done before you get to class, it should take you about 7 hrs!")
-					alert("Good luck!")
-				} else if(brainHurt == "None at all") {
-					alert("Yay! We can welcome you to a REAL headache in just a bit!!!")
-					alert("I'll download some prework ...")
-					alert("Your first class is right now, prework should be done by then!!")
-				}
-			}
-		}
-	}
+  YES - Are you ready to make your brain hurt?
+    YES - Are you sure you want to do that?
+      YES - !Welcome them in!
+        Do you come with experience or inexperience?
+        Experience - Haven't you put your brain through a lot already!?
+        Inexperience - Get ready for a lifetime of headaches, pain, and tears!
+      NO - Okay, there goes your life, we'll be sure to put a close tag />
+    NO - Well, have you had a headache before?
+      Sorry, but I meant a REAL headache, your first day is Feb 27th! HAVE FUN! 
+  NO - Well here's some prework but your first day is tomorrow, have fun!
 */
 
-
-
-/*
 var welcome = alert("Hello! Welcome to your future!")
 
-var name = prompt("My name is Mac, what is your name?")
+var name = prompt("My name is MAC, what is your name?")
 
-var welcome = alert("Welcome to the world of coding " + name +"!")
+var welcome = alert("Welcome to the world of coding " + name + "!")
 
 var positive = confirm("Do you know what you are doing here?")
-    if (positive) {
-        //yes
-        var name = confirm("Are you ready to make your brain hurt?")
 
-       if (positive) {
-            var welcome = confirm("Are you sure you want to keep going?")
+if (positive) {
 
-           if (positive) {
+  var forSure = confirm("Are you ready to make your brain hurt?")
 
-             var brainHurt = prompt("Do you come with experience or none at all?")
+  if (forSure) {
 
-               if (brainHurt == "Experience") {
+    var areCertain = confirm("Are your sure you want to keep going?")
 
-                   alert("OOOOH your poor brain! Okay your first day is in 10 mins!")
+    if (areCertain) {
 
-                   alert(" I'll send you the prework make sure its done before you get to class, it should take you about 7 hrs!")
+      var brainHurt = confirm("Do you come with experience or none at all?")
 
-                   alert("Good luck!")
+      if (brainHurt == true) {
 
-               } else if(brainHurt == "None at all") {
+        alert ("OOOOH your poor brain! Okay your first day is in 10 mins!")
+        alert ("I'll download some prework ...")
+        alert ("Your first class is right now, prework should be done by then!")
 
-                   alert("Yay! We can welcome you to a REAL headache in just a bit!!!")
+      } else if (brainHurt == false) {
 
-                   alert("I'll download some prework ...")
+        alert ("Yay! We can welcome you to a REAL headache in just a bit!!!")
+        alert ("I'll send you the prework make sure it's done before you get to class, it should take you about 7 hrs!")
+        alert ("GOOD LUCK!")
 
-                   alert("Your first class is right now, prework should be done by then!!")
+      }
 
-               }
-            }
-        }
+    } else if (areCertain == false) {
+      alert ("Well here's some prework but your first day is tomorrow, have fun!")
     }
-*/
 
-var welcome = alert("Hello! Welcome to your future!")
+  } else if (forSure == false) {
+    alert ("Your first day is Feb 27th! HAVE FUN!")
+  }
 
-var name = prompt("My name is Mac, what is your name?")
+} else {
+  alert("Okay, there goes your life, we'll be sure to put a close tag />")
+}
 
-var welcome = alert("Welcome to the world of coding " + name +"!")
-
-var positive = confirm("Do you know what you are doing here?")
-    if (positive) {
-        //yes
-        var name = confirm("Are you ready to make your brain hurt?")
-
-       if (positive) {
-            var welcome = confirm("Are you sure you want to keep going?")
-
-           if (positive) {
-
-             var brainHurt = prompt("Do you come with experience or none at all? \n (0) - None at all\n (1) - Experience")
-
-               if (brainHurt == 0) {
-
-                   alert("OOOOH your poor brain! Okay your first day is in 10 mins!")
-
-                   alert(" I'll send you the prework make sure its done before you get to class, it should take you about 7 hrs!")
-
-                   alert("Good luck!")
-
-               } else if (brainHurt == 1) {
-
-                   alert("Yay! We can welcome you to a REAL headache in just a bit!!!")
-
-                   alert("I'll download some prework ...")
-
-                   alert("Your first class is right now, prework should be done by then!!")
-
-               }
-            }
-        }
-    }
 
 
 
